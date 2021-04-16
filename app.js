997745a373a94568eb8aa4471b6598f209dc6fce
BeamerBoy17 <72580478+BeamerBoy17@users.noreply.github.com>
2021-04-16 21:59:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+var myChart = document.getElementById('myChart').getContext('2d');
+
+var Chart = new Chart(myChart, {  
+   
+    type: 'doughnut',
+    data: {
+        labels:[ 'Еда', 'Квартира', 'Здоровье'],
+        datasets: [{ 
+          
+          radius:100,
+          cutout:130,
+          data:  [ 50, 25, 25 ], 
+                
+          backgroundColor: [
+              'rgba(69,90,198,0.2)',
+              'rgba(62,241,210, 0.2)',
+              'rgba(255,84,151, 0.2)',
+          ], 
+          backgroundColor: [
+              'rgb(69,90,198)',
+              'rgb(255,160,93)',
+              'rgb(255,84,151)',
+          ],
+          hoverOffset: 4,
+          borderWidth:0, 
+          fill: false,
+          
+     
+        }]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: true,
+          position: 'right',
+          fontColor:'#FFFFFF',
+        
+        labels: {
+        
+         
+          color:'white',
+          usePointStyle: true,
+          fontColor:'##FFFFFF',
+        
+        font:{  
+          size:14,
+          family:'sans-serif'
+        }
+        
+        },
+        },
+        
+          
+        
+        
+      }
+    },
+  });
